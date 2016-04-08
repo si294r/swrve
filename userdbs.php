72cd7e5169df7f98daef7cc6c2a917780315f9f0
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-04-08 15:55:44 +0700

    Initial Commit

--- userdbs.php
+++ userdbs.php
@@ -28,9 +28,10 @@ function download_file($object) {
             if (strpos($value, "https://") !== FALSE) {
                 $temp = explode("/", $value);
                 $filename = array_pop($temp);
-//                exec("wget --no-check-certificate --directory-prefix=./{$GLOBALS['dir']} "
+
                 exec("wget --no-check-certificate --output-document=./{$GLOBALS['dir']}/$filename "
                         . "\"$value?api_key={$GLOBALS['api_key']}&personal_key={$GLOBALS['personal_key']}\"");
+                exec("gunzip ./{$GLOBALS['dir']}/$filename");
             }
         }
     }
