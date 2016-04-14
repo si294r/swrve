18d4dd78221104bc4f643bd7771cf31070314890
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-04-14 11:58:18 +0700

    Commit

---
+++ userdbs_import.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Function Definition
+ */
+
+function get_table_name($filename) {
+    $pos = strpos($filename, "_");
+    $pos = strpos($filename, "_", $pos + 1);
+    $pos = strpos($filename, "_", $pos + 1);
+    $pos_dot = strpos($filename, ".", $pos);
+    $table_name = substr($filename, $pos + 1, $pos_dot - $pos - 1);
+    return $table_name;
+}
+
+function get_list_filename() {
+    exec("find . -path *userdbs-*.csv", $output); // find all csv
+    asort($output);
+    $output = array_values($output);
+    return $output;
+}
+
+function get_file_sql() {
+    exec("find . -path *mysql.sql", $output); // find all csv
+    return $output[0];
+}
+
+/*
+ * Configuration
+ */
+
+$db_host = 'mariadb_server';
+$db_user = 'root';
+$db_pass = 'password';
+$db_name = 'userdbs';
+
+/*
+ * Main Script
+ */
+$output = array();
+exec("mysql -h $db_host -u $db_user --password=$db_pass -vve \"DROP DATABASE $db_name; CREATE DATABASE $db_name;\"", $output);
+echo implode("\n", $output);
+
+$file_sql = get_file_sql();
+$output = array();
+exec("mysql -h $db_host -u $db_user --password=$db_pass -v $db_name < ", $output);
+echo implode("\n", $output);
+
+die();
+
+$list_filename = get_list_filename();
+//print_r($list_filename);
+foreach ($list_filename as $filename) {
+    $table_name = get_table_name($filename);
+
+    if (in_array($table_name, $drop_table)) {
+    $cmd_drop = <<<EOD
+mysql --local-infile -h $db_host -u $db_user --password=$db_pass -vve "drop table $db_name.$table_name"
+EOD;
+        $output = array();
+        exec($cmd_drop);
+        echo implode("\n", $output);
+    }
+
+    $cmd = <<<EOD
+mysql --local-infile -h $db_host -u $db_user --password=$db_pass -vve "load data local infile '$filename' 
+into table $db_name.$table_name fields terminated by ',' enclosed by '\"' lines terminated by '\n' ignore 1 rows"
+EOD;
+    $output = array();
+    exec($cmd, $output);
+    echo implode("\n", $output);
+    
+    break; // execute one file csv
+}
+
+
