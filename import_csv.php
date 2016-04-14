8d5de49b105e1fb35fad6d8c83bb3768b1d2deb6
chairuddinsyah <chairuddinsyah@10.100.8.149>
2016-04-14 09:47:35 +0700

    Commit

--- import_csv.php
+++ import_csv.php
@@ -33,34 +33,41 @@ $db_name = 'swrve';
  * Main Script
  */
 
-//load data local infile './userdbs-2016-04-12/all-users_02618_2016-04-12_abtest_exposure.1_0.1.csv' into table swrve.abtest_exposure fields terminated by ',' enclosed by '\"' lines terminated by '\n' ignore 1 rows
+//load data local infile './userdbs-2016-04-13/all-users_02618_2016-04-13_abtest_exposure.1_0.1.csv' into table swrve.abtest_exposure fields terminated by ',' enclosed by '\"' lines terminated by '\n' ignore 1 rows
 
-$pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
-$pdo->exec("SET FOREIGN_KEY_CHECKS=0");
-$pdo->exec("PURGE BINARY LOGS BEFORE NOW()");
+//$pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
+//$pdo->exec("SET FOREIGN_KEY_CHECKS=0");
+//$pdo->exec("PURGE BINARY LOGS BEFORE NOW()");
 
 $list_filename = get_list_filename();
 print_r($list_filename);
 foreach ($list_filename as $filename) {
     $table_name = get_table_name($filename);
 
-    $local_filename = "/home/si294r/" . basename($filename);
-    $sql = "
-load data infile '$local_filename' into table $db_name.$table_name 
-fields terminated by ',' enclosed by '\"' lines 
-terminated by '\\n' ignore 1 rows
-";
+//    $local_filename = "/home/si294r/" . basename($filename);
+//    $local_filename = $filename;
+//    $cmd = "load data local infile '$filename' "
+//            . "into table $db_name.$table_name "
+//            . "fields terminated by ',' enclosed by '\"' lines "
+//            . "terminated by '\\n' ignore 1 rows";
 
-    echo $sql;
-    if ($pdo->exec($sql)) {
-        print_r($pdo->errorInfo());
-    } else {
-        $error = $pdo->errorInfo()[2];
-        $error = str_replace("'", "''", $error);
-        $error = str_replace("\\", "\\\\", $error);
-        $pdo->exec("INSERT INTO error_message (table_name, message) VALUES "
-                . "('$table_name', '$error')");
-    }
+    $cmd = <<<EOD
+mysql --local-infile -h $db_host -u $db_user --password=$db_pass -vve "load data local infile '$filename' 
+into table $db_name.$table_name fields terminated by ',' enclosed by '\"' lines terminated by '\n' ignore 1 rows"
+EOD;
+    exec($cmd);
+    
+    
+//    echo $sql;    
+//    if ($pdo->exec($sql)) {
+//        print_r($pdo->errorInfo());
+//    } else {
+//        $error = $pdo->errorInfo()[2];
+//        $error = str_replace("'", "''", $error);
+//        $error = str_replace("\\", "\\\\", $error);
+//        $pdo->exec("INSERT INTO error_message (table_name, message) VALUES "
+//                . "('$table_name', '$error')");
+//    }
 
 //    $i = 0;
 //    $handle = fopen($filename, "r");
