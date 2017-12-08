204d27591f37a7e97d8db42f3914415fbb4a1ade
chairuddinsyah <chairuddinsyah@10.100.8.149>
2017-12-08 18:34:58 +0700

    Update Commit

--- userdbs_import_office.php
+++ userdbs_import_office.php
@@ -68,7 +68,7 @@ $text = "";
 foreach ($list_filename as $filename) {
     $table_name = get_table_name($filename);
     
-    if ($table_name != "swrve_properties_cash_oid") continue;
+    if ($table_name != "swrve_properties") continue;
 
     /*
      * Start Transform CSV from Standard Redshift to Standard Postgres
