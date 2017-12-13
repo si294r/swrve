1dcc8c4c1d87aaa5824e0eeb1de972a3d86ebaa0
chairuddinsyah <chairuddinsyah@10.100.8.149>
2017-12-13 19:44:15 +0700

    Update Commit

--- import_raw_events_office.php
+++ import_raw_events_office.php
@@ -10,34 +10,51 @@ $current_dir = dirname(__FILE__);
 include "$current_dir/../postgres-config.php";
 
 if (isset($argv[2])) {
-    $date = $argv[2];
+//    $date = $argv[2];
+    $startdate = $argv[2];
+    if (isset($argv[3])) {
+        $enddate = $argv[3];
+        if ($enddate < $startdate) {
+            $enddate = $startdate;
+        }
+    } else {
+        $enddate = date('Y-m-d');
+    }
 } else {
-    $date = date('Y-m-d');
+//    $date = date('Y-m-d');
+    $startdate = date('Y-m-d');
+    $enddate = date('Y-m-d');
 }
 
-exec("aws s3 ls s3://swrveexternal-alegrium/app-$swrve_app_id/$date", $output);
+$obj_date = DateTime::createFromFormat('Y-m-d', $startdate);
 
-foreach ($output as $row) {
-    $arr = explode($date, $row);
+while (true) {
 
-    $tableName = "events_{$swrve_app_id}";
-    $tableLogName = "events_{$swrve_app_id}_log";
-    $filename = $date.$arr[2];
-    exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"select * from $tableLogName where filename = '$filename';\"", $out_select);
+    $date = $obj_date->format('Y-m-d');
     
-    if (strpos(implode("\n", $out_select), "(0 rows)") !== false) {
-        echo "$filename not found in $tableLogName".PHP_EOL;
-        echo "downloading...";
-        exec("aws s3 cp s3://swrveexternal-alegrium/app-$swrve_app_id/$filename $current_dir/$filename");
-        echo "extract...";
-        exec("gunzip -f $current_dir/$filename");
-        echo "truncate...";
-        exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"TRUNCATE TABLE temp_json; \"", $out_import);
-        echo "copy...";
-        $logfilename = str_replace(".gz", "", "$current_dir/$filename");
-        exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"\\COPY temp_json FROM '$logfilename'; \"", $out_import);
-        echo "insert...";
-        exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"
+    exec("aws s3 ls s3://swrveexternal-alegrium/app-$swrve_app_id/$date", $output);
+
+    foreach ($output as $row) {
+        $arr = explode($date, $row);
+
+        $tableName = "events_{$swrve_app_id}";
+        $tableLogName = "events_{$swrve_app_id}_log";
+        $filename = $date . $arr[2];
+        exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"select * from $tableLogName where filename = '$filename';\"", $out_select);
+
+        if (strpos(implode("\n", $out_select), "(0 rows)") !== false) {
+            echo "$filename not found in $tableLogName" . PHP_EOL;
+            echo "downloading...";
+            exec("aws s3 cp s3://swrveexternal-alegrium/app-$swrve_app_id/$filename $current_dir/$filename");
+            echo "extract...";
+            exec("gunzip -f $current_dir/$filename");
+            echo "truncate...";
+            exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"TRUNCATE TABLE temp_json; \"", $out_import);
+            echo "copy...";
+            $logfilename = str_replace(".gz", "", "$current_dir/$filename");
+            exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"\\COPY temp_json FROM '$logfilename'; \"", $out_import);
+            echo "insert...";
+            exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"
 insert into events_30088
 select values->>'app_version' as app_version,
        values->>'type' as event_type,
@@ -50,13 +67,19 @@ from
 (
     select values::json as values from   temp_json
 ) a;\"", $out_import);
-        echo PHP_EOL.implode(PHP_EOL, $out_import) .PHP_EOL.PHP_EOL;
-        $out_import = [];
-        
-        exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"Insert into $tableLogName (filename, status) VALUES ('$filename', 'done');\"");
-        
-    } else {
-        echo "$filename found in $tableLogName".PHP_EOL;
+            echo PHP_EOL . implode(PHP_EOL, $out_import) . PHP_EOL . PHP_EOL;
+            $out_import = [];
+
+            exec("psql --host=$rhost --port=$rport --username=$ruser --no-password --echo-all $rdatabase  -c \"Insert into $tableLogName (filename, status) VALUES ('$filename', 'done');\"");
+        } else {
+            echo "$filename found in $tableLogName" . PHP_EOL;
+        }
+//    die;
     }
-    die;
+    
+    if ($obj_date->format('Y-m-d') == $enddate) {
+        break;
+    } else {
+        $obj_date->add(new DateInterval("P1D"));
+    }
 }
